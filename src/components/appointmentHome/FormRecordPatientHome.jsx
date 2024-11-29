a648a8557c69a5e482a534a5a0b48fac764d3fb4
Vu Tien Duc <vutienduc26122002@gmail.com>
2024-11-29 13:51:47 +0700

    1:51 29/111

---
+++ src/components/appointmentHome/FormRecordPatientHome.jsx
@@ -0,0 +1,429 @@
+import { appointmentContext } from '@/context/AppointmentContext';
+import { globalContext, notifyType } from '@/context/GlobalContext';
+import { utilsContext } from '@/context/UtilsContext';
+import { api, TypeHTTP } from "@/utils/api";
+import { convertDateToDayMonthYearVietNam } from "@/utils/date";
+import React, { useContext, useEffect, useState } from 'react';
+const FormRecordPatientHome = ({ medicalRecord, type, setType, setTemporary, doctorRecord1, appointmentHome1, setReload }) => {
+  const { appointmentData, appointmentHandler } = useContext(
+    appointmentContext
+  );
+  const [appointmentHome, setAppointmentHome] = useState()
+  const [doctorRecord, setDoctorRecord] = useState();
+  const [medical, setMedical] = useState([]);
+  const [nameMedical, setNameMedical] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const { utilsHandler } = useContext(utilsContext);
+  const [unitOfCalculation, setUnitOfCalculation] = useState("");
+
+  const [diagnosisDisease, setDiagnosisDisease] = useState("");
+  const [symptoms, setSymptoms] = useState("");
+  const [note, setNote] = useState("");
+  const [reAppointmentDate, setReAppointmentDate] = useState("");
+  const [temperature, setTemperature] = useState(0);
+  const [bloodPressure, setBloodPressure] = useState("");
+  const [healthRate, setHealthRate] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (medicalRecord) {
+      setMedical(medicalRecord.medical)
+      setDiagnosisDisease(medicalRecord.diagnosisDisease)
+      setSymptoms(medicalRecord.symptoms)
+      setNote(medicalRecord.note)
+      setReAppointmentDate(`${medicalRecord.reExaminationDate.year}-${medicalRecord.reExaminationDate.month}-${medicalRecord.reExaminationDate.day}`)
+      setTemperature(medicalRecord.temperature)
+      setBloodPressure(medicalRecord.bloodPressure)
+      setHealthRate(medicalRecord.healthRate)
+      setWeight(medicalRecord.weight)
+      setHeight(medicalRecord.height)
+    }
+  }, [medicalRecord])
+
+  useEffect(() => {
+    setDoctorRecord(doctorRecord1)
+    setAppointmentHome(appointmentHome1)
+
+  }, [doctorRecord1, appointmentHome1, type]);
+
+  // useEffect(() => {
+  //   if (appointmentHome1?.patient) {
+  //     api({ type: TypeHTTP.GET, sendToken: false, path: `/medicalRecords/findByPatient/${appointmentHome1.patient._id}` })
+  //       .then(res => {
+  //         const filter = res.filter(item => item.appointment === appointmentHome1._id)[0]
+  //         if (filter) {
+  //           console.log(filter)
+  //           appointmentHandler.setMedicalRecord(filter)
+  //         }
+  //       })
+  //   }
+  // }, [appointmentHome1])
+
+  // Xử lý
+  // Thêm thuốc
+  const addMedical = () => {
+    const newMedical = {
+      medicalName: nameMedical,
+      quantity: Number(quantity),
+      unitOfCalculation: unitOfCalculation,
+    };
+    setMedical([...medical, newMedical]);
+  };
+
+  useEffect(() => {
+    // Clear input fields after adding a new medical
+    setNameMedical("");
+    setQuantity("");
+    setUnitOfCalculation("Đơn vị tính");
+  }, [medical]);
+
+  const updateMedicalRecord = () => {
+    setTemporary(true)
+
+    // ham de huy
+    const reject = () => {
+      setTemporary(false)
+    }
+
+    // ham de update hay lam cai gi do khi dong y
+    const accept = () => {
+      let splitDate = reAppointmentDate
+        ? reAppointmentDate.split("-")
+        : [];
+      // chổ này cần check xem có nhập đủ thông tin không
+      if (symptoms === "") {
+        utilsHandler.notify(notifyType.WARNING, "Chưa nhập triệu chứng")
+      }
+      if (diagnosisDisease === "") {
+        utilsHandler.notify(notifyType.WARNING, "Chưa nhập chẩn đoán bệnh")
+      }
+      if (note === "") {
+        utilsHandler.notify(notifyType.WARNING, "Chưa nhập ghi chú")
+      }
+      if (medical.length === 0) {
+        utilsHandler.notify(notifyType.WARNING, "Chưa nhập đơn thuốc")
+      }
+      const body = {
+        patient: appointmentHome?.patient?._id,
+        doctor: doctorRecord?.doctor?._id,
+        diagnosisDisease: diagnosisDisease,
+        symptoms: symptoms,
+        note: note,
+        reExaminationDate: {
+          day: splitDate[2] || "",
+          month: splitDate[1] || "",
+          year: splitDate[0] || "",
+        },
+        temperature: temperature,
+        bloodPressure: bloodPressure,
+        healthRate: healthRate,
+        weight: weight,
+        height: height,
+        medical: medical,
+        appointment: appointmentHome?._id,
+        date: appointmentHome?.appointment_date,
+      };
+      utilsHandler.notify(notifyType.LOADING, "Đang lưu hồ sơ bệnh nhân")
+      setTemporary(false);
+      api({
+        path: "/medicalRecords/save",
+        type: TypeHTTP.POST,
+        sendToken: false,
+        body,
+      }).then(async (medicalRecord) => {
+        await api({
+          path: `/medicalRecords/send-mail/${medicalRecord._id}`,
+          type: TypeHTTP.POST,
+          sendToken: false,
+        })
+          .then(res => {
+            // thông báo thành công
+            setReload(prev => !prev)
+            utilsHandler.notify(notifyType.SUCCESS, "Đã lưu hồ sơ bệnh nhân")
+            setType(0)
+          })
+      });
+    }
+
+    utilsHandler.showSure({
+      message: 'Cập nhật hồ sơ bệnh nhân sẽ không thể sửa đổi. Bác sĩ chắc chắn chứ?',
+      functionalAccept: accept,
+      functionalReject: reject
+    })
+  };
+
+  return (
+    <div className="px-[2rem] min-w-[100%] h-full py-[1rem] flex flex-col gap-2">
+      <div className='flex items-center'>
+        <i onClick={() => setType(0)} className='bx bx-chevron-left text-[30px] cursor-pointer text-[#565656]'></i>
+        <span className="font-semibold">Hồ Sơ Bệnh Án</span>
+      </div>
+      <div className="flex items-center justify-between py-1 px-2 w-full">
+        <div className="flex items-center gap-4 ">
+          <div
+            className="w-[50px] aspect-square rounded-full shadow-xl"
+            style={{
+              backgroundSize: "cover",
+              backgroundImage: `url(${appointmentHome?.patient?.image})`,
+            }}
+          ></div>
+          <div className="flex flex-col">
+            <span className="font-medium">
+              {appointmentHome?.patient?.fullName}
+            </span>
+            <span className="text-[14px]">
+              {appointmentHome?.patient?.phone}
+            </span>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          {/* <div
+              className="w-[50px] aspect-square rounded-full shadow-xl"
+              style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${doctorRecord?.doctor?.image})`,
+              }}
+            ></div> */}
+          <div className="flex flex-col text-[14px]">
+            <span className="text-[14px] font-semibold">
+              Khám Bệnh Tại Nhà
+            </span>
+
+            <span className="self-end">
+              {/* {convertDateToDayMonthYearVietNam(
+                    date
+                  )} */}
+            </span>
+
+          </div>
+        </div>
+      </div>
+      <span>
+        <span className="font-semibold px-2">
+          Triệu Chứng:
+        </span>{" "}
+        <input
+          disabled={medicalRecord ? true : false}
+          className="text-[14px] w-[500px] h-[40px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+          onChange={(e) => setSymptoms(e.target.value)}
+          value={symptoms}
+        />
+      </span>
+      <div className="grid grid-cols-3 h-auto gap-[0.25rem]">
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Huyết áp:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            className="text-[14px] w-[80px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) =>
+              setBloodPressure(e.target.value)
+            }
+            value={bloodPressure}
+          />
+          mmHg
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Nhịp tim:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            className="text-[14px] w-[80px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) => setHealthRate(e.target.value)}
+            value={healthRate}
+          />
+          bpm
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Cân nặng:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            className="text-[14px] w-[80px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) => setWeight(e.target.value)}
+            value={weight}
+          />
+          kg
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Chiều cao:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            className="text-[14px] w-[80px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) => setHeight(e.target.value)}
+            value={height}
+          />
+          cm
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Nhiệt độ:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            className="text-[14px] w-[80px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) =>
+              setTemperature(e.target.value)
+            }
+            value={temperature}
+          />
+          ℃
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold px-2">
+            Tái khám:
+          </span>
+          <input
+            disabled={medicalRecord ? true : false}
+            type="date"
+            className="text-[14px] w-[150px] h-[30px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+            onChange={(e) =>
+              setReAppointmentDate(e.target.value)
+            }
+            value={reAppointmentDate}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 h-auto gap-x-[0.5rem] px-2">
+        <textarea
+          disabled={medicalRecord ? true : false}
+          placeholder="Chuẩn đoán bệnh"
+          className="text-[14px] w-[100%] h-[90px] py-2 bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+          onChange={(e) =>
+            setDiagnosisDisease(e.target.value)
+          }
+          value={diagnosisDisease}
+        ></textarea>
+        <textarea
+          disabled={medicalRecord ? true : false}
+          placeholder="Lời dặn bác sĩ"
+          className="text-[14px] w-[100%] h-[90px] py-2 bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+          onChange={(e) => setNote(e.target.value)}
+          value={note}
+        ></textarea>
+      </div>
+      <span className="font-semibold px-2">
+        Đơn Thuốc
+      </span>
+      <div className="grid grid-cols-2 h-auto gap-x-[0.25rem] px-2">
+        {!medicalRecord && (
+          <div className="text-[14px] w-[100%] focus:outline-0 rounded-lg px-4">
+            <input
+              placeholder="Tên thuốc"
+              className="text-[14px] w-[100%] h-[40px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+              onChange={(e) =>
+                setNameMedical(e.target.value)
+              }
+              value={nameMedical}
+            />
+            <div className="flex items-center justify-between">
+              <select
+                className="text-[14px] mt-2 w-[48%] h-[40px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+                value={unitOfCalculation}
+                onChange={(e) =>
+                  setUnitOfCalculation(e.target.value)
+                }
+              >
+                <option>Đơn vị tính</option>
+                <option>Viên</option>
+                <option>Vỉ</option>
+                <option>Hộp</option>
+                <option>Ống</option>
+                <option>Gói</option>
+                <option>Chai/Lọ</option>
+                <option>Tuýp</option>
+              </select>
+              <input
+                placeholder="Số lượng"
+                className="text-[14px] mt-2 w-[48%] h-[40px] bg-[white] border-[1px] border-[#cfcfcf] focus:outline-0 rounded-lg px-4"
+                onChange={(e) =>
+                  setQuantity(e.target.value)
+                }
+                value={quantity}
+              />
+            </div>
+            {!appointmentData.medicalRecord && (
+              <button
+                className="hover:scale-[1.05] transition-all text-[14px] bg-[blue] flex justify-center items-center w-[30%] text-[white] mt-2 h-[37px] rounded-lg"
+                onClick={() => addMedical()}
+              >
+                Thêm
+              </button>
+            )}
+
+          </div>
+        )}
+        <div className="w-full max-h-[140px] overflow-y-auto relative">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="sticky top-0 left-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th
+                  scope="col"
+                  className="w-[10%] py-2 text-center"
+                >
+                  #
+                </th>
+                <th
+                  scope="col"
+                  className="w-[50%] py-2 text-center"
+                >
+                  Tên Thuốc
+                </th>
+                <th scope="col" className="w-[20%] py-2">
+                  Số Lượng
+                </th>
+                <th scope="col" className="w-[20%] py-2">
+                  Đơn vị tính
+                </th>
+              </tr>
+            </thead>
+            <tbody className=" w-[full] bg-black font-medium">
+              {medical.map((medical, index) => (
+                <tr
+                  key={index}
+                  className="odd:bg-white cursor-pointer hover:bg-[#eee] transition-all odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                >
+                  <td
+                    scope="row"
+                    className="px-6 py-2 text-center font-medium"
+                  >
+                    {index + 1}
+                  </td>
+                  <td className="py-2 text-[15px] text-center">
+                    {medical.medicalName}
+                  </td>
+                  <td className="py-2">
+                    {medical.quantity}
+                  </td>
+                  <td className="py-2">
+                    {medical.unitOfCalculation}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="full flex justify-end">
+        {!medicalRecord && (
+          <button
+            onClick={() => updateMedicalRecord()}
+            className="hover:scale-[1.05] transition-all bg-[blue] text-[white] text-[15px] font-medium px-4 rounded-md py-2"
+          >
+            Cập Nhật Hồ Sơ
+          </button>
+        )}
+      </div>
+
+
+    </div>
+  );
+}
+
+export default FormRecordPatientHome
---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,172 @@
+"use client";
+import ChatBot from "@/components/chatbot/ChatBot";
+import Notification from "@/components/notification";
+import MessageIcon from "@/components/shortcut/MessageIcon";
+import { api, TypeHTTP } from "@/utils/api";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { appointmentContext } from "./AppointmentContext";
+import { userContext } from "./UserContext";
+export const globalContext = createContext();
+
+export const notifyType = {
+  SUCCESS: "success",
+  FAIL: "fail",
+  WARNING: "warning",
+  LOADING: "loading",
+  HEALTH: "health",
+  NONE: "none",
+};
+
+const GlobalProvider = ({ children }) => {
+  const [info, setInfo] = useState({
+    status: notifyType.NONE,
+    message: "",
+  });
+  const { userHandler, userData } = useContext(userContext);
+  const pathname = usePathname();
+  const router = useRouter();
+  const { appointmentHandler, appointmentData } =
+    useContext(appointmentContext);
+  const notify = (status, message) =>
+    setInfo({ status, message });
+
+  const reload = () => {
+    setTimeout(() => {
+      globalThis.window.location.reload();
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (info.status !== notifyType.NONE) {
+      if (info.status !== notifyType.LOADING) {
+        setTimeout(() => {
+          setInfo({ status: notifyType.NONE, message: "" });
+        }, 3000);
+      }
+    }
+  }, [info.status]);
+
+  useEffect(() => {
+    api({
+      type: TypeHTTP.GET,
+      sendToken: true,
+      path: "/auth/userByToken",
+    })
+      .then((user) => {
+        if (user.role === "ADMIN") {
+          notify(
+            notifyType.FAIL,
+            "Tài khoản của bạn không đủ quyền để truy cập"
+          );
+          globalThis.localStorage.removeItem("accessToken");
+          globalThis.localStorage.removeItem(
+            "refreshToken"
+          );
+        } else {
+          userHandler.setUser(user);
+        }
+      })
+      .catch((error) => {
+        globalThis.localStorage.removeItem("accessToken");
+        globalThis.localStorage.removeItem("refreshToken");
+        // notify(notifyType.FAIL, error.message)
+      });
+  }, [pathname]);
+
+  useEffect(() => {
+    const accessToken = globalThis.localStorage.getItem('accessToken')
+    const pathnames = [
+      "ho-so",
+      "cam-nang",
+      "chi-tiet-cam-nang",
+      "zero",
+      "cong-dong",
+      "chi-tiet-cau-hoi",
+      "cuoc-tro-chuyen",
+    ];
+    const patientPathnames = [
+      "/cuoc-hen-cua-ban"
+    ];
+    const doctorPathname = [
+      "/phieu-dang-ky",
+      "/ho-so-ca-nhan-bac-si",
+      "/them-cam-nang",
+      "/benh-nhan-cua-toi",
+      "/thong-ke-doanh-thu",
+      "/doanh-thu-cua-toi",
+    ];
+    if (pathname.includes("location")) {
+
+    } else {
+      if (userData.user) {
+        console.log
+        //nếu là admin
+        if (userData.user.role === "ADMIN") {
+          notify(
+            notifyType.FAIL,
+            "Vui Lòng đăng nhập bằng tài khoản người sử dụng"
+          );
+          userHandler.setUser();
+          globalThis.localStorage.removeItem("accessToken");
+          globalThis.localStorage.removeItem("refreshToken");
+        }
+
+        // nêú là patient
+        else if (userData.user.role === "USER") {
+          console.log(111)
+          if (doctorPathname.includes(pathname)) {
+            // router.push("/");
+          }
+        }
+
+        // nếu là doctor
+        else {
+          if (patientPathnames.includes(pathname)) {
+            router.push("/phieu-dang-ky");
+          }
+          if (pathnames.includes(pathname.split("/")[1])) {
+            router.push("/phieu-dang-ky");
+          }
+        }
+      } else {
+        if (
+          doctorPathname.includes(pathname) ||
+          patientPathnames.includes(pathname)
+        ) {
+          if (!accessToken) {
+            router.push("/");
+          }
+        }
+      }
+    }
+  }, [pathname, userData.user]);
+
+  const data = {};
+  const handler = {
+    notify,
+    reload,
+  };
+
+  return (
+    <globalContext.Provider
+      value={{ globalData: data, globalHandler: handler }}
+    >
+      <Notification
+        status={info.status}
+        message={info.message}
+        setInfomation={setInfo}
+      />
+      {!pathname.includes("zero") && <ChatBot />}
+      {children}
+    </globalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
