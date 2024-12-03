898298a24d68f3f3f7fa3e729dad14b1c740ad37
Thai Bao <boishiro2002@gmail.com>
2024-12-03 16:16:51 +0700

    update tham gia cuoc hen patient 3/12

--- src/app/(patient)/cuoc-hen-cua-ban/page.jsx
+++ src/app/(patient)/cuoc-hen-cua-ban/page.jsx
@@ -56,42 +56,14 @@ const CuocHenCuaBan = () => {
   }, []);
 
   useEffect(() => {
-    if (appointments.length > 0) {
-      const theFirstAppointment = sortByAppointmentDate(
-        appointments.filter(
-          (item) => item.status === "ACCEPTED"
-        )
-      ).filter((item) =>
-        compareTimeDate1GreaterThanDate2(
-          item.appointment_date,
-          convertDateToDayMonthYearTimeObject(
-            new Date().toISOString()
-          )
-        )
-      )[0];
-      if (theFirstAppointment) {
-        if (
-          compare2Date(
-            convertDateToDayMonthYearTimeObject(
-              new Date().toISOString()
-            ),
-            theFirstAppointment.appointment_date
-          )
-        ) {
-          if (
-            isALargerWithin10Minutes(
-              theFirstAppointment.appointment_date.time,
-              time
-            ) ||
-            isALargerWithin60Minutes(
-              time,
-              theFirstAppointment.appointment_date.time
-            )
-          ) {
-            setDisplayConnect(theFirstAppointment._id);
+    if (appointments.length > 0) {  /* sửa ở đây */
+      sortByAppointmentDate(appointments.filter((item) => item.status === "ACCEPTED")).forEach((item) => {
+        if (compare2Date(convertDateToDayMonthYearTimeObject(new Date().toISOString()), item.appointment_date)) {
+          if (isALargerWithin10Minutes(item.appointment_date.time, time) || isALargerWithin60Minutes(time, item.appointment_date.time)) {
+            setDisplayConnect(item._id);
           }
         }
-      }
+      })
     }
   }, [appointments, time]);
 
